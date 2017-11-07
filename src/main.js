ee8f1746b8d22e4017710b02a48c86955453fd02
Winson Sun <winsonsun@gmail.com>
2017-11-07 10:39:10 +0800

    24 - optmization for store

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import * as firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+import DateFilter from './filters/date'
+import AlertCmp from './components/Shared/Alert.vue'
+import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog.vue'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog.vue'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog.vue'
+import RegisterDialog from './components/Meetup/Registration/RegisterDialog.vue'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.filter('date', DateFilter)
+Vue.component('app-alert', AlertCmp)
+Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
+Vue.component('app-meetup-register-dialog', RegisterDialog)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDN5_NHLo1AqUkuHqO_LiplM5LWPUeHuC8',
+      authDomain: 'startervue.firebaseapp.com',
+      databaseURL: 'https://startervue.firebaseio.com',
+      projectId: 'startervue',
+      storageBucket: 'gs://startervue.appspot.com'
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+        this.$store.dispatch('fetchUserData')
+      }
+    })
+    this.$store.dispatch('loadMeetups')
+  }
+})
